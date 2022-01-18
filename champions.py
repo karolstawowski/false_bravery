30c5546de818bd61198c5cbece638c7d1f188e1f
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2022-01-18 18:50:56 +0100

    refactor: change for loops to inside-list loops

--- champions.py
+++ champions.py
@@ -3,14 +3,11 @@ import requests
 
 
 def get_champions_from_api(lol_version: str) -> list:
-    champions = []
-
     response_en = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{lol_version}/data/en_US/champion.json")
 
     resp_en = response_en.json()['data']
 
-    for id in resp_en:
-        champions.append(id)
+    champions = [id for id in resp_en]
 
     return champions
 
--- images.py
+++ images.py
@@ -12,25 +12,25 @@ def create_temp_directory():
         os.makedirs('temp')
 
 
-def generate_template_image(path: str) -> Image:
-    if os.path.isfile(f'{path}'):
-        return Image.open(f'{path}')
+def generate_template_image(template_image_path: str) -> Image:
+    if os.path.isfile(f'{template_image_path}'):
+        return Image.open(f'{template_image_path}')
     image = Image.new('RGB', (370, 644), (80, 80, 80))
-    image.save(f'{path}')
+    image.save(f'{template_image_path}')
     return image
 
 
-def create_champion_label(champion_name: str, image: Image):
-    draw_image = ImageDraw.Draw(image)
+def create_champion_label(champion_name: str, template_image: Image):
+    draw_image = ImageDraw.Draw(template_image)
     font = ImageFont.truetype("./assets/Roboto-Bold.ttf", 20)
     w, h = draw_image.textsize(f"{champion_name}", font=font)
     draw_image.text(((120 - w) / 2 + 20, 150), f"{champion_name}", (255, 255, 255), font=font)
 
 
-def create_skill_order_label(skills: list, template_image: Image):
+def create_skill_order_label(skills_list: list, template_image: Image):
     draw_image = ImageDraw.Draw(template_image)
     font = ImageFont.truetype("./assets/Roboto-Bold.ttf", 24)
-    draw_image.text((240, 102), f"{skills[0]} > {skills[1]} > {skills[2]} ", (255, 255, 255), font=font)
+    draw_image.text((240, 102), f"{skills_list[0]} > {skills_list[1]} > {skills_list[2]} ", (255, 255, 255), font=font)
 
 
 def create_item_label(item_label: str, template_image: Image, x_axis: int, y_axis: int):
@@ -42,7 +42,6 @@ def create_item_label(item_label: str, template_image: Image, x_axis: int, y_axi
 def generate_image(champion: str, boots_item: Item, mythic_item: Item, legendary_items: list,
                    summoner_spell_1: SummonerSpell, summoner_spell_2: SummonerSpell, skill_order: list,
                    primary_rune: PrimaryRune, rune_tree: RuneTree):
-    legendary_items_array = []
     item_image_size = 64
     champion_image_size = 120
     line_height = 20
@@ -51,22 +50,21 @@ def generate_image(champion: str, boots_item: Item, mythic_item: Item, legendary
 
     create_temp_directory()
 
-    # get images
+    # Get images
 
     champion_image = get_image_from_api(champion, 'champion')
     boots_image = get_image_from_api(boots_item.id, 'item')
     mythic_item_image = get_image_from_api(mythic_item.id, 'item')
-    for item in legendary_items:
-        legendary_items_array.append(get_image_from_api(item.id, 'item'))
+    legendary_items_images = [get_image_from_api(item.id, 'item') for item in legendary_items]
     summoner_spell_images = get_image_from_api('spell0', 'sprite')
     rune_image = get_rune_image_from_api(primary_rune.key, primary_rune.image_link, (64, 64), (0, 0, 0))
     rune_tree_image = get_rune_image_from_api(rune_tree.key, rune_tree.image_link, (48, 48), (60, 60, 60))
 
-    # prepare template image
+    # Prepare template image
 
     template_image = generate_template_image("./assets/template.png")
 
-    # insert images
+    # Insert images
 
     template_image.paste(champion_image, (outer_padding, outer_padding))
 
@@ -88,18 +86,19 @@ def generate_image(champion: str, boots_item: Item, mythic_item: Item, legendary
 
     legendary_item_i = 0
     while legendary_item_i < 4:
-        template_image.paste(legendary_items_array[legendary_item_i], (
-            outer_padding,
-            champion_image_size + 2 * outer_padding + inside_padding + item_image_size * (
-                    legendary_item_i + 2) + line_height + inside_padding * (
-                    legendary_item_i + 2)))
+        template_image.paste(legendary_items_images[legendary_item_i], (outer_padding,
+                                                                        champion_image_size + 2 * outer_padding +
+                                                                        inside_padding + item_image_size * (
+                                                                                legendary_item_i + 2) + line_height +
+                                                                        inside_padding * (
+                                                                                legendary_item_i + 2)))
         legendary_item_i += 1
 
     template_image.paste(rune_image, (230, 26))
 
     template_image.paste(rune_tree_image, (304, 34))
 
-    # insert labels
+    # Insert labels
 
     create_champion_label(champion, template_image)
 
