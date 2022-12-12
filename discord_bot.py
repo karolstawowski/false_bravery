0ad999dddda66b2e2f882725250aa0981bb64e87
Karol Stawowski <stawowski.dev@gmail.com>
2022-12-12 23:16:46 +0100

    fix: prohibit bot from reacting to @everyone Discord message

--- discord_bot.py
+++ discord_bot.py
@@ -1,11 +1,13 @@
 import discord
+
 from champions import randomize_champion
+from config import LEAGUE_OF_LEGENDS_VERSION
 from images import generate_image
-from items import randomize_legendary_items, randomize_boots, randomize_mythic_item
+from items import (randomize_boots, randomize_legendary_items,
+                   randomize_mythic_item)
 from runes import randomize_primary_rune, randomize_rune_tree
 from skills import randomize_skill_order
 from summoner_spells import randomize_summoner_spells
-from config import LEAGUE_OF_LEGENDS_VERSION
 
 
 def get_bot_password():
@@ -26,6 +28,9 @@ def runDiscordBot(mythic_items, legendary_items, boots_items, champions, summone
         if message.author == client.user:
             return
 
+        if message.mention_everyone:
+            return
+
         if message.content.startswith('!aramki'):
             random_legendary_items = randomize_legendary_items(legendary_items)
             random_summoner_spells = randomize_summoner_spells(summoner_spells)
@@ -34,21 +39,25 @@ def runDiscordBot(mythic_items, legendary_items, boots_items, champions, summone
             random_skill_order = randomize_skill_order()
             random_champion = randomize_champion(champions)
             random_primary_rune = randomize_primary_rune(primary_runes)
-            random_rune_tree = randomize_rune_tree(random_primary_rune, rune_trees)
+            random_rune_tree = randomize_rune_tree(
+                random_primary_rune, rune_trees)
 
             generate_image(random_champion, random_boots,
                            random_mythic_item,
                            random_legendary_items,
                            summoner_spells[random_summoner_spells[0]],
-                           summoner_spells[random_summoner_spells[1]], random_skill_order, random_primary_rune,
+                           summoner_spells[random_summoner_spells[1]
+                                           ], random_skill_order, random_primary_rune,
                            random_rune_tree, LEAGUE_OF_LEGENDS_VERSION)
 
             await message.reply(file=discord.File("./temp/output_file.png"))
 
         if client.user.mentioned_in(message):
             embed = discord.Embed(title="Oxygen's Bot Help")
-            embed.add_field(name="Available commands:", value="!aramki - generate random stuff")
-            embed.set_footer(text="https://github.com/karolstawowski/false_bravery")
+            embed.add_field(name="Available commands:",
+                            value="!aramki - generate random stuff")
+            embed.set_footer(
+                text="https://github.com/karolstawowski/false_bravery")
             await message.channel.send(embed=embed)
 
     client.run(password)
