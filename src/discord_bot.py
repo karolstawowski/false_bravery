841a2530571b0eaa17a1e9391e08550a685e7886
Karol Stawowski <stawowski.dev@gmail.com>
2023-12-09 20:31:37 +0100

    chore: extract send image function

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -1,12 +1,7 @@
 import discord
 
-from champions import randomize_champion
 from config import LEAGUE_OF_LEGENDS_VERSION
-from images import generate_image
-from items import randomize_boots, randomize_legendary_items
-from runes import randomize_primary_rune, randomize_rune_tree
-from skills import randomize_skill_order
-from summoner_spells import randomize_summoner_spells
+from send_image import send_image
 
 
 def get_bot_password():
@@ -35,27 +30,16 @@ def run_discord_bot(
             return
 
         if message.content.startswith(RUN_COMMAND_KEYWORD):
-            random_legendary_items = randomize_legendary_items(legendary_items)
-            random_summoner_spells = randomize_summoner_spells(summoner_spells)
-            random_boots = randomize_boots(boots_items)
-            random_skill_order = randomize_skill_order()
-            random_champion = randomize_champion(champions)
-            random_primary_rune = randomize_primary_rune(primary_runes)
-            random_rune_tree = randomize_rune_tree(random_primary_rune, rune_trees)
-
-            generate_image(
-                random_champion,
-                random_boots,
-                random_legendary_items,
-                summoner_spells[random_summoner_spells[0]],
-                summoner_spells[random_summoner_spells[1]],
-                random_skill_order,
-                random_primary_rune,
-                random_rune_tree,
+            return await send_image(
+                message,
                 LEAGUE_OF_LEGENDS_VERSION,
+                legendary_items,
+                boots_items,
+                champions,
+                summoner_spells,
+                primary_runes,
+                rune_trees,
             )
-
-            await message.reply(file=discord.File("./temp/output_file.png"))
 
         if client.user.mentioned_in(message):
             embed = discord.Embed(title="Oxygen's Bot Help")
---
+++ src/send_image.py
@@ -0,0 +1,40 @@
+import discord
+from champions import randomize_champion
+from images import generate_image
+from items import randomize_boots, randomize_legendary_items
+from runes import randomize_primary_rune, randomize_rune_tree
+from skills import randomize_skill_order
+from summoner_spells import randomize_summoner_spells
+
+
+async def send_image(
+    message,
+    lol_version,
+    legendary_items,
+    boots_items,
+    champions,
+    summoner_spells,
+    primary_runes,
+    rune_trees,
+):
+    random_legendary_items = randomize_legendary_items(legendary_items)
+    random_summoner_spells = randomize_summoner_spells(summoner_spells)
+    random_boots = randomize_boots(boots_items)
+    random_skill_order = randomize_skill_order()
+    random_champion = randomize_champion(champions)
+    random_primary_rune = randomize_primary_rune(primary_runes)
+    random_rune_tree = randomize_rune_tree(random_primary_rune, rune_trees)
+
+    generate_image(
+        random_champion,
+        random_boots,
+        random_legendary_items,
+        summoner_spells[random_summoner_spells[0]],
+        summoner_spells[random_summoner_spells[1]],
+        random_skill_order,
+        random_primary_rune,
+        random_rune_tree,
+        lol_version,
+    )
+
+    return await message.reply(file=discord.File("./temp/output_file.png"))
