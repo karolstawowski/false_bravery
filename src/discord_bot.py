1ffaac293d5af6c9ced98c7fdfc210342d508b85
Karol Stawowski <stawowski.dev@gmail.com>
2023-12-16 11:13:34 +0100

    feat: add discord command autocomplete

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -1,11 +1,12 @@
 import discord
+from discord.ext import commands
 
 from config import LEAGUE_OF_LEGENDS_VERSION
-from send_image import send_image
+from prepare_image import prepare_image
 
 
-def get_bot_password():
-    return open("bot_password.txt", "r").read()
+def get_bot_token():
+    return open("../bot_password.txt", "r").read()
 
 
 RUN_COMMAND_KEYWORD = "!aramki"
@@ -14,12 +15,15 @@ RUN_COMMAND_KEYWORD = "!aramki"
 def run_discord_bot(
     legendary_items, boots_items, champions, summoner_spells, primary_runes, rune_trees
 ):
-    client = discord.Client()
-    password = get_bot_password()
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = commands.Bot(command_prefix="!", intents=intents)
+    BOT_TOKEN = get_bot_token()
 
     @client.event
     async def on_ready():
         print("We have logged in as {0.user}".format(client))
+        await client.tree.sync()
 
     @client.event
     async def on_message(message):
@@ -29,9 +33,8 @@ def run_discord_bot(
         if message.mention_everyone:
             return
 
-        if message.content.startswith(RUN_COMMAND_KEYWORD):
-            return await send_image(
-                message,
+        if message.content == RUN_COMMAND_KEYWORD:
+            await prepare_image(
                 LEAGUE_OF_LEGENDS_VERSION,
                 legendary_items,
                 boots_items,
@@ -40,6 +43,8 @@ def run_discord_bot(
                 primary_runes,
                 rune_trees,
             )
+
+            return await message.reply(file=discord.File("../temp/output_file.png"))
 
         if client.user.mentioned_in(message):
             embed = discord.Embed(title="Oxygen's Bot Help")
@@ -50,4 +55,20 @@ def run_discord_bot(
             embed.set_footer(text="https://github.com/karolstawowski/false_bravery")
             await message.channel.send(embed=embed)
 
-    client.run(password)
+    @client.tree.command(
+        name="aramki",
+        description="Generate a random set of items, runes, summoner spells and skill order.",
+    )
+    async def _space(ctx: discord.interactions.Interaction):
+        await prepare_image(
+            LEAGUE_OF_LEGENDS_VERSION,
+            legendary_items,
+            boots_items,
+            champions,
+            summoner_spells,
+            primary_runes,
+            rune_trees,
+        )
+        await ctx.response.send_message(file=discord.File("../temp/output_file.png"))
+
+    client.run(BOT_TOKEN)
